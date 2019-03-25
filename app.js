f6f9b60fc40eeffce1d77101b8c8da05f341fc4d
Irene <uaireneua@gmail.com>
2019-03-25 18:35:22 +0200

    add slider

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// add to cart
+
 var productsCount = document.getElementById ("products-count");
 // console.log (productsCount);
 
@@ -14,3 +16,55 @@ for (var i = 0;  i < addToCartButtons.length; i++) {
 	}) 
 	
 }
+
+// add like
+
+var likedButtons = document.querySelectorAll(".heart");
+console.log (likedButtons);
+
+
+for (var i = 0; i < addToCartButtons.length; i++) {
+	likedButtons[i].addEventListener ("click", function () {
+	if (this.classList.contains("liked")) {
+		this.classList.remove ("liked")
+	} else 
+		this.classList.add("liked")
+	})
+
+}
+
+// slick slider
+
+$('.slider').slick( {
+	dots: true,
+	autoplay:true,
+	autoplaySpeed:10000,
+})
+
+// quantity input
+
+var decrementButton = document.querySelectorAll(".decrement-button") [0];
+var quantityInput = document.querySelectorAll(".quantity-input") [0];
+var incrementButton = document.querySelectorAll(".increment-button") [0];
+
+// console.log (quantityInput.value)
+
+    // vidkluchennya knopky
+
+    
+
+incrementButton.addEventListener ("click", function (){
+
+	var currentValue = +quantityInput.value
+    var nextValue = currentValue + 1
+    // console.log (nextValue)
+    quantityInput.value = nextValue
+})
+
+decrementButton.addEventListener ("click", function (){
+
+	var currentValue = +quantityInput.value
+    var nextValue = currentValue - 1
+    // console.log (nextValue)
+    quantityInput.value = nextValue
+})
